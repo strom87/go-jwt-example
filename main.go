05df3caeb2b94ad7385993ac0330044b6ff6bcb6
Daniel Ström <daniel.strom.87@gmail.com>
2015-08-03 20:07:44 +0200

    added main.go file

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"crypto/rsa"
+	"fmt"
+	"github.com/codegangsta/negroni"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gorilla/mux"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+func main() {
+
+}
